853266425b8afe3547ebbaee2c62c49982c2e749
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-12-22 22:37:13 -0300

    feat: create async functions crud service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findall() {
+    return await this.todoRepository.find();
+  }
+
+  async findOneOrFail(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw new NotFoundError(error.message);
+    }
+  }
+
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOneOrFail(id);
+
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.findOneOrFail(id);
+    await this.todoRepository.softDelete(id);
+  }
+}
