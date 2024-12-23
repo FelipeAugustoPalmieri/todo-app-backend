9b2f68a7cf415df09d79fe3c480c13e7f6c24d92
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-12-22 23:09:14 -0300

    fix: change route controller

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -12,7 +12,7 @@ import {
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 
-@Controller('api/v1/todo')
+@Controller('api/v1/todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
