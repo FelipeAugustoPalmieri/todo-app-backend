babf90cf2de25f6e48a9113a280d55d06f320779
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-12-25 23:41:03 -0300

    feat: add apiResponse and apiOperation in routes

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -13,27 +13,63 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IndexTodoSwagger } from '../swagger/index-todo.swagger';
 
 @Controller('api/v1/todos')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List all tasks' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved task list.',
+    type: IndexTodoSwagger,
+    isArray: true,
+  })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
   async index() {
     return await this.todoService.findall();
   }
 
   @Post()
+  @ApiOperation({ summary: 'Add a new task' })
+  @ApiResponse({ status: 201, description: 'Task successfully created.' })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid data format or missing required fields.',
+  })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
   async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Search for a task' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task successfully retrieved.',
+  })
+  @ApiResponse({ status: 404, description: 'Task not found.' })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a task' })
+  @ApiResponse({
+    status: 200,
+    description: 'Task successfully updated.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid data format or missing required fields.',
+  })
+  @ApiResponse({ status: 404, description: 'Task not found.' })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() body: UpdateTodoDto,
@@ -42,6 +78,13 @@ export class TodoController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a task' })
+  @ApiResponse({
+    status: 204,
+    description: 'Task successfully deleted. No content returned.',
+  })
+  @ApiResponse({ status: 404, description: 'Task not found.' })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.todoService.deleteById(id);
